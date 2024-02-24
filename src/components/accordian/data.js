16de167f8fd2aba08fd0f74e107b090f53e43608
Sonu <sonumrx37@gmail.com>
2024-02-24 21:01:12 +0530

    accordian created

---
+++ src/components/accordian/data.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    id: 1,
+    question: "What is the capital of France?",
+    answer:
+      "The capital of France is Paris. It is known as the 'City of Light'.",
+  },
+  {
+    id: 2,
+    question: "Who wrote 'Romeo and Juliet'?",
+    answer:
+      "William Shakespeare wrote 'Romeo and Juliet'. It is one of his most famous plays.",
+  },
+  {
+    id: 3,
+    question: "What is the largest planet in our solar system?",
+    answer:
+      "Jupiter is the largest planet in our solar system. It is a gas giant.",
+  },
+  {
+    id: 4,
+    question: "Who painted the Mona Lisa?",
+    answer:
+      "Leonardo da Vinci painted the Mona Lisa. It is one of the most famous paintings in the world.",
+  },
+];
+
+export default data;
