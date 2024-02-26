9412980a291feec981f26c11ef5c2ec3e3098306
Sonu <sonumrx37@gmail.com>
2024-02-26 20:44:16 +0530

    Created Image slider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import './App.css';
-import Accordian from './components/accordian';
-import RandomColor from './components/random-color';
-import StarRating from './components/star-rating';
+import "./App.css";
+import Accordian from "./components/accordian";
+import ImageSlider from "./components/image-slider";
+import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
 
 function App() {
   return (
@@ -11,9 +12,15 @@ function App() {
       {/* Random color component */}
       {/* <RandomColor /> */}
 
-
       {/* Star Rating Component */}
-      <StarRating noOfStars={10}/>
+      {/* <StarRating noOfStars={10}/> */}
+
+      {/* Image slider component */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"4"}
+      />
     </div>
   );
 }
